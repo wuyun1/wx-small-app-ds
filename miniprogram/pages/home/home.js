1b07d2888b33dee3e18feb4a776dfb6d840d1efb
13485 <yunqiang.wu@hand-china.com>
2019-10-15 22:02:02 +0800

    Initial Commit

---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,7 @@
+"use strict";
+Page({
+    data: {},
+    onLoad: function () {
+    },
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaG9tZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImhvbWUudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLElBQUksQ0FBQztJQUNILElBQUksRUFBRSxFQUNMO0lBQ0QsTUFBTTtJQUNOLENBQUM7Q0FDRixDQUFDLENBQUEiLCJzb3VyY2VzQ29udGVudCI6WyJQYWdlKHtcbiAgZGF0YToge1xuICB9LFxuICBvbkxvYWQoKSB7XG4gIH0sXG59KVxuIl19
