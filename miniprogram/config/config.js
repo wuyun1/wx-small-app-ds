6bdbdaced7c296d62c584f51159ed8044b1e7115
13485 <yunqiang.wu@hand-china.com>
2019-11-03 14:44:48 +0800

    update: remove unless file

--- miniprogram/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var config = {
-    appkey: 'gUgOl5iEyXSrYStD',
-    apiBaseUrl: 'https://se-7yue.jd1.jajabjbj.top/v1'
-};
-exports.config = config;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY29uZmlnLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQ0EsSUFBTSxNQUFNLEdBQUc7SUFDWCxNQUFNLEVBQUUsa0JBQWtCO0lBQzFCLFVBQVUsRUFBRSxxQ0FBcUM7Q0FDcEQsQ0FBQztBQUdFLHdCQUFNIiwic291cmNlc0NvbnRlbnQiOlsiXG5jb25zdCBjb25maWcgPSB7XG4gICAgYXBwa2V5OiAnZ1VnT2w1aUV5WFNyWVN0RCcsXG4gICAgYXBpQmFzZVVybDogJ2h0dHBzOi8vc2UtN3l1ZS5qZDEuamFqYWJqYmoudG9wL3YxJ1xufTtcblxuZXhwb3J0IHtcbiAgICBjb25maWdcbn1cbiJdfQ==
